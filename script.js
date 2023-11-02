2c2a15b552a7a6c54db2767f586c8bba9b9a4fc9
zzhhaa <zzhhaa@protonmail.com>
2023-11-02 11:13:19 +0000

    worm-basic

--- script.js
+++ script.js
@@ -1,80 +1,125 @@
 // info modal
 
 // open the instructions modal
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems);
+//   });
   
-  // close the modal when button clicked
-  document.getElementById('closeModal').addEventListener('click', function () {
-    var modalInstance = M.Modal.getInstance(document.getElementById('instructionsModal'));
-    modalInstance.close();
-  });
+//   // close the modal when button clicked
+//   document.getElementById('closeModal').addEventListener('click', function () {
+//     var modalInstance = M.Modal.getInstance(document.getElementById('instructionsModal'));
+//     modalInstance.close();
+//   });
 
-// game variables
+// board
+var blockSize = 25;
+var rows = 25;
+var cols = 25;
+var board;
+var context;
 
-let dry = 0;
-let timer = 30;
+// worm
+var wormX = blockSize * 5;
+var wormY = blockSize * 5;
 
-// task notifications 
+var velocityX = 0;
+var velocityY = 0;
 
-// winning and losing conditions
+var wormBody = [];
 
-// timer
-// Select the timer container element
-const timerContainer = document.querySelector(".timer");
+// food
+var foodX;
+var foodY;
 
-// Create a CountdownTimer instance
-const countdownTimer = new CountdownTimer();
+// end conditions
+var gameOver = false;
 
-// Append the timer's HTML content to the container
-timerContainer.innerHTML = CountdownTimer.getHTML();
+window.onload = function() {
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d");
 
-class CountdownTimer {
-  constructor() {
-    this.el = {
-      seconds: document.querySelector(".seconds"),
-    }
+  placeFood();
+  document.addEventListener("keyup", changeDirection)
+  setInterval(update, 1000/10); // reloads 10 times a second
+}
 
-    this.interval = null;
-    this.remainingSeconds = 30;
+function update() {
+  // draw board
+  context.fillStyle="saddlebrown";
+  context.fillRect(0, 0, board.width, board.height);
 
-    this.el.closeModal.addEventListener("click", () {
-      if (this.interval === null) {
-        this.start();
-      } else {
-        this.stop();
-      }
-    });
+  // draw food
+  context.fillStyle = "yellow";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
 
-    updateInterfaceTime() {
-      this.el.seconds.textContent = remainingSeconds.toString().padStart(2,"0");
-    }
-
-    start() {
-      if (this.remainingSeconds === 0) return;
-
-      this.interval = setInterval(() => {
-        this.remainingSeconds--;
-        this.updateInterfaceTime();
-
-        if (this.remainingSeconds === 0) {
-          this.stop();
-        }
-      }, 1000);
-    }
-
-    stop() {
-      clearInterval(this.interval);
-      this.interval = null;
-    }
-
-    static getHTML() {
-      return `
-        <span class="seconds">00</span>
-      `;
-    }
-    
+  // eat food
+  if (wormX === foodX && wormY === foodY) {
+    wormBody.push([foodX, foodY]);
+    placeFood();
   }
+
+  // worm body attaches to worm head
+  for (let i = wormBody.length - 1; i > 0; i--) {
+    wormBody[i] = wormBody[i-1];
+  }
+  if (wormBody.length) {
+    wormBody[0] = [wormX, wormY];
+  }
+
+  // draw worm
+  context.fillStyle = "salmon";
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  wormX += velocityX * blockSize;
+  wormY += velocityY * blockSize;
+  context.fillRect(wormX, wormY, blockSize, blockSize);
+  context.strokeRect(wormX, wormY, blockSize, blockSize);
+
+  for (let i = 0; i < wormBody.length; i++) {
+    context.fillRect(wormBody[i][0], wormBody[i][1], blockSize, blockSize);
+    context.strokeRect(wormBody[i][0], wormBody[i][1], blockSize, blockSize);
+  }
+
+  // end condition
+  if (wormX < 0 || wormX > cols * blockSize || wormY < 0 || wormY > rows * blockSize) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < wormBody.length; i++) {
+    if (wormX === wormBody[i][0] && wormY === wormBody[i][1])
+    gameOver = true;
+  }
+
+  // handle end of game
+  if (gameOver) {
+    alert("Game over! Click 'OK' to play again.");
+    window.location.reload();
+  }
+}
+
+function changeDirection(e) {
+  if (e.code == "ArrowUp" && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  }
+  else if (e.code == "ArrowDown" && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  }
+  else if (e.code == "ArrowLeft" && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  }
+  else if (e.code == "ArrowRight" && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
+ 
+function placeFood() {
+  foodX = Math.floor(Math.random() * rows) * blockSize;
+  foodY = Math.floor(Math.random() * cols) * blockSize;
 }
